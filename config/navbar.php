50603c589405a363212eecba2ba9b0872ace1dee
gusman widodo <gusmanwidodo@gmail.com>
2016-01-19 13:53:20 +0800

    fix conflict

---
+++ config/navbar.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    "dashboard" => [
+
+        "dashboard" => "dashboard",
+        "enquiries" => "enquiries"
+    ],
+
+    "properties" => [
+
+        "properties" => "manage properties",
+        "property/categories" => "categories"
+
+    ],
+
+    "customers" => [
+
+        "customers" => "manage customers"
+
+    ],
+
+    "blog" => [
+
+        "blog" => "blog",
+        "blog-categories" => "categories",
+        "blog-comments" => "comments",
+        "blog-settings" => "settings"
+    ],
+
+    "settings" => [
+
+        "accounts" => "admin accounts",
+        "settings" => "settings",
+        "index" => "index management",
+        "currency" => "currency",
+        "notifications" => "notifications",
+        "io" => "import/export",
+        "about" => "about"
+    ]
+];
