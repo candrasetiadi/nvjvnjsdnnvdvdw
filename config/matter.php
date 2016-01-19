674b6fd85e8d3facfce3d6fe53d0e16d14438561
gusman widodo <gusmanwidodo@gmail.com>
2016-01-19 13:45:10 +0800

    .gitignore is now working

--- config/matter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	
-	'langs' => [
-		'en',
-		'id'
-	]
-];
--- resources/views/admin/fragments/footscripts.blade.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript" src="{{ asset('bower/jquery/dist/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower/tinymce/tinymce.jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower/lodash/lodash.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower/medium-editor/dist/js/medium-editor.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower/nprogress/nprogress.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/admin/scripts.js') }}"></script>
