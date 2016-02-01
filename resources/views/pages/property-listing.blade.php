50afc698dec0b60c760afc815c07dd6908562f5a
gusman widodo <gusmanwidodo@gmail.com>
2016-02-01 16:12:17 +0800

    frontend: fix url in listing item

--- resources/views/pages/property-listing.blade.php
+++ resources/views/pages/property-listing.blade.php
@@ -6,7 +6,13 @@
 
         @foreach($properties as $property)
         <div class="col-md-4 list-item">
-            <a href="{{ route( Lang::get('url')[$property->category->route] . '/' . str_slug($property->lang()->title) . '-' . $property->id) }}">
+            <a href="{{ route('property.' . $property->category->route, 
+                [
+                    $property->category->route => Lang::get('url')[$property->category->route],
+                    'property' => str_slug($property->lang()->title) . '-' . $property->id
+
+                ]) }}">
+
                 <div class="thumbnail">
                     @if(count($property->propertyFiles) > 0)
                     <img src="{{ asset('uploads/property/' . $property->propertyFiles[0]->file) }}">
