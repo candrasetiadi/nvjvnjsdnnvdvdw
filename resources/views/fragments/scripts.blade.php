cfb4ebfa4222358642fbe544877a55d09b8aa1f6
gusman widodo <gusmanwidodo@gmail.com>
2016-02-01 09:39:33 +0800

    fix conflict

--- resources/views/fragments/scripts.blade.php
+++ resources/views/fragments/scripts.blade.php
@@ -1,12 +1,12 @@
 <script src="http://maps.googleapis.com/maps/api/js?libraries=geometry,places"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="{{ asset('assets/js/scripts.js') }}"></script>
-<script src="{{ asset('assets/js/markerwithlabel.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap-slider.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.event.move.js') }}"></script>
-<script src="{{ asset('assets/js/responsive-slider.js') }}"></script>
+<script src="assets/js/scripts.js"></script>
+<script src="assets/js/markerwithlabel.js"></script>
+<script src="assets/js/bootstrap-slider.js"></script>
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/jquery.event.move.js"></script>
+<script src="assets/js/responsive-slider.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+<script src="assets/js/bootstrap.js"></script>
 
